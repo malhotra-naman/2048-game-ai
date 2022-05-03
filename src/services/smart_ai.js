8f7664c8939be8a0c5c1db0cdba4331ef499364e
MK954481 <mayankkumar_co20b5_04@dtu.ac.in>
2022-05-04 02:27:18 +0530

    Smart AI Update

--- src/services/smart_ai.js
+++ src/services/smart_ai.js
@@ -10,6 +10,18 @@ SmartAI = function (game) {
 };
 
 SmartAI.prototype.nextMove = function () {
+  var goal = this.determineGoal(this.game.grid);
+  while (goal.type != GoalType.MOVE) {
+    goal = this.determineSubGoal(this.game.grid, goal);
+  }
+
+  if (goal.directions) {
+    for (var i = 0; i < goal.directions.length; i++) {
+      if (this.game.moveAvailable(goal.directions[i])) {
+        return goal.directions[i];
+      }
+    }
+  }
   var originalQuality = this.gridQuality(this.game.grid);
   var results = this.planAhead(this.game.grid, 3, originalQuality);
   var bestResult = this.chooseBestMove(results, originalQuality);
@@ -154,4 +166,131 @@ SmartAI.prototype.gridQuality = function (grid) {
 
   var score = monoScore + emptyScore;
   return score;
+};
+SmartAI.prototype.determineGoal = function (grid) {
+  var goal = new Goal();
+  var maxValue = 0;
+  var maxCells = [];
+  grid.eachCell(function (x, y, tile) {
+    if (tile && tile.value >= maxValue) {
+      if (tile.value > maxValue) {
+        maxCells = [];
+        maxValue = tile.value;
+      }
+      maxCells.push({ x: x, y: y });
+    }
+  });
+  var maxCell;
+  if (maxCells.length == 1) {
+    maxCell = maxCells[0];
+  } else {
+    var minDist = grid.size;
+    for (var i = 0; i < maxCells.length; i++) {
+      dist =
+        Math.min(maxCells[i].x, grid.size - maxCells[i].x - 1) +
+        Math.min(maxCells[i].y, grid.size - maxCells[i].y - 1);
+      if (dist < minDist) {
+        minDist = dist;
+        maxCell = maxCells[i];
+      }
+    }
+  }
+  dist =
+    Math.min(maxCell.x, grid.size - maxCell.x - 1) +
+    Math.min(maxCell.y, grid.size - maxCell.y - 1);
+  if (dist == 0) {
+    goal.type = GoalType.BUILD;
+    goal.cell = maxCell;
+    goal.value = maxValue * 2;
+    return goal;
+  }
+  if (dist == 1) {
+    if (maxValue <= 512) {
+      goal.type = GoalType.BUILD;
+      goal.cell = {
+        x: maxCell.x < grid.size / 2 ? 0 : grid.size - 1,
+        y: maxCell.y < grid.size / 2 ? 0 : grid.size - 1,
+      };
+      goal.value = maxValue;
+      return goal;
+    }
+    var availableCells = game.grid.availableCells();
+    if (availableCells.length > 4) {
+      goal.type = GoalType.SHIFT;
+      goal.fromCell = maxCell;
+      if (maxCell.x == 0 || maxCell.x == game.size - 1) {
+        goal.cell = {
+          x: maxCell.x,
+          y: maxCell.y < grid.size / 2 ? grid.size - 1 : 0,
+        };
+      } else {
+        goal.cell = {
+          x: maxCell.x < grid.size / 2 ? grid.size - 1 : 0,
+          y: maxCell.y,
+        };
+      }
+      return goal;
+    }
+    goal.type = GoalType.BUILD;
+    goal.cell = maxCell;
+    goal.value = maxValue * 2;
+    return goal;
+  }
+  var availableCells = game.grid.availableCells();
+  goal.type = GoalType.SHIFT;
+  goal.fromCell = maxCell;
+  goal.cell = {
+    x: maxCell.x,
+    y: maxCell.y < grid.size / 2 ? grid.size - 1 : 0,
+  };
+  return goal;
+};
+
+SmartAI.prototype.determineSubGoal = function (grid, goal) {
+  var subgoal = new Goal();
+  if (goal.type == GoalType.BUILD) {
+    var tile = grid.cellContent(goal.cell);
+
+    if (!tile) {
+      goal.type = GoalType.MOVE;
+      vector = {
+        x: goal.cell.x - grid.size / 2,
+        y: goal.cell.y - grid.size / 2,
+      };
+      goal.directions = this.getDirections(vector);
+      return goal;
+    }
+
+    for (i = 0; i < 4; i++) {
+      var vector = this.game.getVector(i);
+      var adjCell = { x: goal.cell.x + vector.x, y: goal.cell.y + vector.y };
+      var adjTile = grid.cellContent(adjCell);
+      if (adjTile && adjTile.value == tile.value) {
+        vector.x = -vector.x;
+        vector.y = -vector.y;
+        goal.type = GoalType.MOVE;
+        goal.directions = this.getDirections(vector);
+        return goal;
+      }
+    }
+  } else if (goal.type == GoalType.MOVE) {
+  }
+  return subgoal;
+};
+
+SmartAI.prototype.getDirections = function (vector) {
+  directions = [0, 3, -1, -1];
+  if (vector.x > 0) {
+    directions[0] = 2;
+  }
+  if (vector.y > 0) {
+    directions[1] = 1;
+  }
+  if (Math.abs(vector.x) > Math.abs(vector.y)) {
+    var temp = directions[0];
+    directions[0] = directions[1];
+    directions[1] = temp;
+  }
+  directions[2] = (directions[1] + 2) % 4;
+  directions[3] = (directions[0] + 2) % 4;
 };
